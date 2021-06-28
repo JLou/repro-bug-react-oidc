94e47550b161044debb37731b6d872c6f9386553
Jean-Lou PIERME <jeanlou.pierme@axa.fr>
2021-06-28 11:07:11 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { withAuthentication } from "@axa-fr/react-oidc-context-fetch";
+import logo from "./logo.svg";
+import "./App.css";
+import { OidcSecure } from "@axa-fr/react-oidc-context";
+import {
+  AuthenticationProvider,
+  oidcLog,
+  InMemoryWebStorage,
+} from "@axa-fr/react-oidc-context";
+import { useEffect } from "react";
+
+const configuration = {
+  client_id: "interactive.public.short",
+  redirect_uri: "http://localhost:3000/authentication/callback",
+  response_type: "code",
+  post_logout_redirect_uri: "http://localhost:3000/",
+  scope: "openid profile email api offline_access",
+  authority: "https://demo.identityserver.io",
+  silent_redirect_uri: "http://localhost:3000/authentication/silent_callback",
+  automaticSilentRenew: true,
+  loadUserInfo: true,
+};
+
+let i = 0;
+
+const getI = () => {
+  i++;
+  return i;
+};
+
+const Composant = ({ fetch, user }) => {
+  useEffect(() => console.log("fetch effect"), [fetch]);
+  useEffect(() => console.log("rerender"), []);
+  return <h1>toto {getI()}</h1>;
+};
+
+Composant.whyDidYouRender = true;
+
+const EnhancedComposant = withAuthentication(fetch)(Composant);
 
 function App() {
   return (
@@ -17,6 +54,16 @@ function App() {
         >
           Learn React
         </a>
+        <AuthenticationProvider
+          configuration={configuration}
+          loggerLevel={oidcLog.DEBUG}
+          isEnabled={false}
+          UserStore={InMemoryWebStorage}
+        >
+          <OidcSecure>
+            <EnhancedComposant />
+          </OidcSecure>
+        </AuthenticationProvider>
       </header>
     </div>
   );
